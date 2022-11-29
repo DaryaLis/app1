c92d179e03231f0210ca479b8110420d79c38c21
DaryaLis <21dara02@gmail.com>
2022-11-29 21:19:47 +0300

    выводит в консоль

---
+++ src/App.jsx
@@ -0,0 +1,51 @@
+/*import { useState } from 'react'
+import reactLogo from './assets/react.svg'*/
+
+/*function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+      </div>
+
+    </div>
+  )
+}*/
+
+import React from 'react'
+
+function App () {
+
+let likes = 0; /*создали счетчик кол-ва лайков*/
+
+function increment() {
+  likes += 1;
+  console.log(likes);
+};
+
+function decrement() {
+  likes -= 1;
+  console.log(likes);
+};
+
+  return (
+    <div className="App">
+<h1>{likes}</h1>
+
+    <button onClick={increment}>Increment</button>
+    <button onClick={decrement}>Decrement</button>
+    </div>
+  )
+};
+
+export default App
+
+ /* НЕ JSX синтаксис!
+    React.createElement('button', {
+        onClick: () => console.log('CLICK')
+    }, 'Нажми на меня!'), */
